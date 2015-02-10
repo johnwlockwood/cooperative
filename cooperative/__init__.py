c9522af52d4ff4236cb7883dcea26163e115c4dc
johnwlockwood <johnwlockwood@gmail.com>
2015-02-09 21:40:25 -0800

    Ignore trial's temporary directory
    Add tests for cooperative accumulators
    
    Add functions to test to demonstrate catching errors
    
    Add cooperative accumulator
    
    Add batch cooperative accumulator

--- cooperative/__init__.py
+++ cooperative/__init__.py
@@ -1,5 +1,7 @@
+# _*_ coding: utf-8 _*_
+
 import sys
-from . import _meta
+from cooperative import _meta
 
 __version__ = _meta.version
 __version_info__ = _meta.version_info
@@ -8,4 +10,60 @@ __version_info__ = _meta.version_info
 def is_py3():
     return sys.version > '3'
 
+from twisted.internet.task import cooperate
 
+from karld.tap import Bucket
+from karld.tap import stream_tap
+from karld.iter_utils import i_batch
+
+
+def cooperative_accumulation_handler(stopped_generator, spigot):
+    """
+    Drain the contents of the bucket from the spigot.
+
+    :param stopped_generator: Generator which as stopped
+    :param spigot: a Bucket.
+    :return: The contents of the bucket.
+    """
+    return spigot.drain_contents()
+
+
+def cooperative_accumulate(a_generator_func):
+    """
+    Start a Deferred whose callBack arg is a deque of the accumulation
+    of the values yielded from a_generator_func.
+
+    :param a_generator_func: A function which returns a generator.
+    :return: A Deferred to which the next callback will be called with
+     the yielded contents of the generator function.
+    """
+
+    spigot = Bucket(lambda x: x)
+    items = stream_tap((spigot,), a_generator_func())
+    d = cooperate(items).whenDone()
+    d.addCallback(cooperative_accumulation_handler, spigot)
+    return d
+
+
+def cooperative_accumulate_batched(max_batch_size, a_generator_func):
+    """
+    Start a Deferred whose callBack arg is a deque of the accumulation
+    of the values yielded from a_generator_func which is iterated over
+    in batches the size of max_batch_size.
+
+    It should be more efficient to iterate over the generator in
+     batches and still provide enough speed for non-blocking execution.
+
+    :param max_batch_size: The number of iterations of the generator
+     to consume at a time.
+    :param a_generator_func: A function which returns a generator.
+    :return: A Deferred to which the next callback will be called with
+     the yielded contents of the generator function.
+    """
+    from twisted.internet.task import cooperate
+    spigot = Bucket(lambda x: x)
+    items = stream_tap((spigot,), a_generator_func())
+
+    d = cooperate(i_batch(max_batch_size, items)).whenDone()
+    d.addCallback(cooperative_accumulation_handler, spigot)
+    return d
---
+++ cooperative/tests/test_cooperative.py
@@ -0,0 +1,324 @@
+# _*_ coding: utf-8 _*_
+from collections import deque
+from functools import partial
+from itertools import chain
+
+from twisted.internet import defer
+from twisted.internet.defer import inlineCallbacks
+from twisted.python import log
+from twisted.trial import unittest
+
+from karld.tap import Bucket
+
+from cooperative import cooperative_accumulation_handler
+from cooperative import cooperative_accumulate
+from cooperative import cooperative_accumulate_batched
+
+
+class TestHandler(unittest.TestCase):
+    def test_cooperative_accumulation_handler(self):
+        """
+        Ensure the return value of cooperative_accumulation_handler
+        is the contents of a Bucket instance with it's contents
+        drained.
+
+        :return:
+        """
+        spigot = Bucket(unicode)
+        spigot(1)
+        spigot(2)
+
+        result = cooperative_accumulation_handler(None, spigot)
+        self.assertEqual(result, deque([u"1", u"2"]))
+        self.assertEqual(spigot.contents(), deque([]))
+
+
+def i_get_tenth_11(value):
+    """
+    Yield the tenth and eleventh item of value.
+
+    :param value:
+    :return:
+    """
+    yield value[10]
+    yield value[11]
+
+
+
+@inlineCallbacks
+def run_some_with_error(value):
+    """
+    Cooperatively iterator over two iterators consecutively, but
+     the second one will always raise an IndexError, which is caught,
+     logged and a message is returned.
+
+    :return:
+    """
+    #  first deferred
+    result = yield cooperative_accumulate(partial(i_get_tenth_11, value))
+
+    try:
+        result2 = yield cooperative_accumulate(partial(i_get_tenth_11, result))
+        defer.returnValue(result2)
+    except IndexError, e:
+        log.err(e)
+        defer.returnValue("Couldn't get second result, "
+                          "but the first result is {}".format(result))
+
+
+@inlineCallbacks
+def run_some_without_error(value):
+    """
+    Cooperatively iterator over two iterators consecutively and the
+    result of the final one is returned.
+
+    :param value: Any sequence.
+    :return:
+    """
+    #  first deferred
+    result = yield cooperative_accumulate(partial(i_get_tenth_11, list(range(110, 150))))
+
+    log.msg("accumulated {}".format(result))
+    result = yield cooperative_accumulate(partial(i_get_tenth_11, value))
+    defer.returnValue(result)
+
+
+class TestAccumulate(unittest.TestCase):
+    @inlineCallbacks
+    def test_cooperative_accumulate(self):
+        """
+        Ensure that within an inline callback function,
+        a cooperative_accumulate wrapped generator
+        yields the result of the output of the generator.
+
+        :return:
+        """
+        result = yield cooperative_accumulate(partial(i_get_tenth_11, list(range(110, 150))))
+        self.assertEqual(result, deque([120, 121]))
+
+
+    @inlineCallbacks
+    def test_failure(self):
+        """
+        Ensure that within an inline callback function,
+        a cooperative_accumulate based function
+        yields the result if it's cooperative generator.
+
+        Since and_the_winner_is is designed to always
+        log and error, Ensure one IndexError is logged.
+
+        :return:
+        """
+        result = yield run_some_with_error(list(range(15)))
+        self.assertEqual(result, "Couldn't get second result, "
+                                 "but the first result is deque([10, 11])")
+
+        # failure expected
+        index_errors = self.flushLoggedErrors(IndexError)
+        self.assertEqual(len(index_errors), 1)
+
+    @inlineCallbacks
+    def test_multi_winner(self):
+        """
+        Ensure multiple inline callback functions will run cooperatively.
+
+        :return:
+        """
+        d1 = run_some_without_error(list(range(15)))
+        d2 = run_some_without_error(list(range(15, 100)))
+        result = yield defer.gatherResults([d1, d2])
+
+        self.assertEqual(result, [deque([10, 11]), deque([25, 26])])
+
+    @inlineCallbacks
+    def test_trice_winner(self):
+        """
+        Ensure multiple inline callback functions will run cooperatively.
+
+        :return:
+        """
+        d1 = run_some_without_error(list(range(15)))
+        d2 = run_some_without_error(list(range(15, 100)))
+        d3 = cooperative_accumulate(partial(i_get_tenth_11, list(range(4, 50))))
+        result = yield defer.gatherResults([d1, d2, d3])
+
+        self.assertEqual(result, [deque([10, 11]),
+                                  deque([25, 26]),
+                                  deque([14, 15])])
+
+    @inlineCallbacks
+    def test_multi_winner_chain(self):
+        """
+        Ensure multiple inline callback functions will run cooperatively.
+
+        Ensure the result of gatherResults can be chained together
+        in order.
+
+        :return:
+        """
+        called = []
+
+        def watcher(value):
+            """
+            A pass through generator for i_get_tenth_11 that
+            captures the value in a list, which can show the
+            order of the generator iteration.
+
+            :param value:
+            :return:
+            """
+            for item in i_get_tenth_11(value):
+                called.append(item)
+                yield item
+
+        result = yield defer.gatherResults([
+            cooperative_accumulate(partial(watcher, list(range(0, 15)))),
+            cooperative_accumulate(partial(watcher, list(range(15, 100)))),
+            cooperative_accumulate(partial(watcher, list(range(98, 200)))),
+            cooperative_accumulate(partial(watcher, list(range(145, 189))))
+        ])
+
+        final_result = list(chain.from_iterable(result))
+
+        self.assertEqual(final_result, [10, 11, 25, 26, 108, 109, 155, 156])
+        self.assertEqual(set(called), set(final_result))
+        self.assertNotEqual(called, final_result)
+        #  The iteration is shown to alternate between generators passed
+        #   to cooperate.
+        self.assertEqual(called, [10, 25, 108, 155, 11, 26, 109, 156])
+
+    @inlineCallbacks
+    def test_multi_deux_chain(self):
+        """
+        Ensure multiple inline callback functions will run cooperatively.
+
+        Ensure the result of gatherResults can be chained together
+        in order.
+
+        Ensure cooperatively run generators will complete
+        no matter the length.
+
+        Ensure the longest one will continue to iterate after the
+        others run out of iterations.
+
+        :return:
+        """
+        called = []
+
+        def watcher(value):
+            """
+            A pass through generator for i_get_tenth_11 that
+            captures the value in a list, which can show the
+            order of the generator iteration.
+
+            :param value:
+            :return:
+            """
+            for item in i_get_tenth_11(value):
+                called.append(item)
+                yield item
+
+        def deux_watcher(value):
+            """
+            A pass through generator for i_get_tenth_11 that
+            captures the value in a list, which can show the
+            order of the generator iteration.
+
+            :param value:
+            :return:
+            """
+            for item in i_get_tenth_11(value):
+                called.append(item)
+                yield item
+            for item in i_get_tenth_11(value):
+                called.append(item)
+                yield item
+
+        result = yield defer.gatherResults([
+            cooperative_accumulate(partial(watcher, list(range(0, 15)))),
+            cooperative_accumulate(partial(watcher, list(range(15, 100)))),
+            cooperative_accumulate(partial(deux_watcher,
+                                           list(range(1098, 10200)))),
+            cooperative_accumulate(partial(watcher, list(range(145, 189))))
+        ])
+
+        final_result = list(chain.from_iterable(result))
+
+        self.assertEqual(final_result,
+                         [10, 11, 25, 26, 1108, 1109, 1108, 1109, 155, 156])
+        self.assertEqual(set(called), set(final_result))
+        self.assertNotEqual(called, final_result)
+        #  The iteration is shown to alternate between generators passed
+        #   to cooperate.
+        self.assertEqual(called,
+                         [10, 25, 1108, 155, 11, 26, 1109, 156, 1108, 1109])
+
+    @inlineCallbacks
+    def test_multi_deux_batched(self):
+        """
+        Ensure multiple inline callback functions will run cooperatively.
+
+        Ensure the result of gatherResults can be chained together
+        in order.
+
+        Ensure cooperatively run generators will complete
+        no matter the length.
+
+        Ensure the longest one will continue to iterate after the
+        others run out of iterations.
+
+        Ensure those called with cooperative_accumulate_batched will
+        iterate over the generator in batches the size of max_size.
+
+        :return:
+        """
+        called = []
+
+        def watcher(value):
+            """
+            A pass through generator for i_get_tenth_11 that
+            captures the value in a list, which can show the
+            order of the generator iteration.
+
+            :param value:
+            :return:
+            """
+            for item in i_get_tenth_11(value):
+                called.append(item)
+                yield item
+
+        def deux_watcher(value):
+            """
+            A pass through generator for i_get_tenth_11 that
+            captures the value in a list, which can show the
+            order of the generator iteration.
+
+            :param value:
+            :return:
+            """
+            for item in i_get_tenth_11(value):
+                called.append(item)
+                yield item
+            for item in i_get_tenth_11(value):
+                called.append(item)
+                yield item
+
+        result = yield defer.gatherResults([
+            cooperative_accumulate(partial(watcher, list(range(0, 15)))),
+            cooperative_accumulate(partial(watcher, list(range(15, 100)))),
+            cooperative_accumulate_batched(3, partial(deux_watcher,
+                                           list(range(1098, 10200)))),
+            cooperative_accumulate(partial(watcher, list(range(145, 189))))
+        ])
+
+        final_result = list(chain.from_iterable(result))
+
+        self.assertEqual(final_result,
+                         [10, 11, 25, 26, 1108, 1109, 1108, 1109, 155, 156])
+        self.assertEqual(set(called), set(final_result))
+        self.assertNotEqual(called, final_result)
+        #  The iteration is shown to alternate between generators passed
+        #   to cooperate, except the batched one does three at a time
+        #   when it is it's turn.
+        self.assertEqual(called,
+                         [10, 25, 1108, 1109, 1108, 155, 11, 26, 1109, 156])

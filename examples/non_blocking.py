74d711abf1afd0eed3e3eb29172c3e8584348c9b
johnwlockwood <johnwlockwood@gmail.com>
2015-02-11 00:01:01 -0800

    Add short non-blocking example to readme
    Enhance examples to show out of phase
    responding to request by running a computation.

--- examples/non_blocking.py
+++ examples/non_blocking.py
@@ -40,31 +40,46 @@ def expensive2(number):
 @defer.inlineCallbacks
 def do_some_expensive_things(number):
     """
+    Perform one expensive computation cooperatively with any
+     other iterator passed into twisted's cooperate, then
+     use it's result to pass into the second computation.
 
     :param number:
     :return:
     """
     result = yield batch_accumulate(1000, expensive(number))
-    log.msg("first for {}: {}".format(number, reduce(add, result, 0)))
-    result = yield batch_accumulate(1000, expensive2(number+1))
-    log.msg("second for {}: {}".format(number, reduce(add, result, 0)))
+    total = reduce(add, result, 0)
+    log.msg("first for {}: {}".format(number, total))
+
+    result = yield batch_accumulate(1000, expensive2(int(total/1e9)))
+    total = reduce(add, result, 0)
+    log.msg("second for {}: {}".format(number, total))
+    defer.returnValue(total)
+
+
+@defer.inlineCallbacks
+def do_less_expensive_things(number):
+    """
+    Perform one expensive computation cooperatively with any
+     other iterator passed into twisted's cooperate, then
+     use it's result to pass into the second computation.
+
+    :param number:
+    :return:
+    """
+    result = yield batch_accumulate(1000, expensive(number))
+    total = reduce(add, result, 0)
+    log.msg("only for {}: {}".format(number, total))
+    defer.returnValue(total)
 
 
 def main(reactor):
-    """
-
-
-    :param reactor:
-    :return:
-    """
-    total = 0
-
     d1 = do_some_expensive_things(54.0)
     d2 = do_some_expensive_things(42)
     d3 = do_some_expensive_things(10)
     d4 = do_some_expensive_things(34)
 
-    # Enqueue events to simulate
+    # Enqueue events to simulate handling external events
     d5 = defer.Deferred().addCallback(log.msg)
     reactor.callLater(0.3, d5.callback, "########## simulated request 1 ############")
 
@@ -74,7 +89,13 @@ def main(reactor):
     d7 = defer.Deferred().addCallback(log.msg)
     reactor.callLater(1.0, d7.callback, "########## simulated request 3 ############")
 
-    return defer.gatherResults([d1, d2, d3, d4, d5, d6, d7])
+    # simulate an external event triggering an expensive computation while
+    # other expensive computations are happening.
+    d8 = defer.Deferred()
+    d8.addCallback(do_less_expensive_things)
+    reactor.callLater(0.3, d8.callback, 20001)
+
+    return defer.gatherResults([d1, d2, d3, d4, d5, d6, d7, d8]).addCallback(log.msg)
 
 if __name__ == "__main__":
     log.startLogging(sys.stdout)
